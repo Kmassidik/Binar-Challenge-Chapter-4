c6e2f215926c899302bc19dc06c5939482206160
Kurnia Massidik <k.massidik@gmail.com>
2022-08-08 20:26:40 +0700

    masih ancur

--- index.js
+++ index.js
@@ -42,18 +42,83 @@ class pickOption {
 }
 function pickOptionPlayer(params) {
     let set = new pickOption();
-    const collection = document.getElementsByClassName("option");
+    set.playerName = "player";
+    set.playerOption = params;
+    let collection = document.getElementsByClassName("player");
     if (params == "batu") {
         collection[0].style.backgroundColor = "red";
+        collection[1].style.backgroundColor = "transparent";
+        collection[2].style.backgroundColor = "transparent";
     } else if (params == "gunting") {
+        collection[0].style.backgroundColor = "transparent";
         collection[1].style.backgroundColor = "red";
+        collection[2].style.backgroundColor = "transparent";
     }else{
+        collection[0].style.backgroundColor = "transparent";
+        collection[1].style.backgroundColor = "transparent";
         collection[2].style.backgroundColor = "red";
     }
-    set.playerName = "player"
-    set.playerOption = params;
     set.botOption = set.randomBot();
+    let collectionBot = document.getElementsByClassName("bot");
+    if (set.botOption == "batu") {
+        collectionBot[0].style.backgroundColor = "red";
+        collectionBot[1].style.backgroundColor = "transparent";
+        collectionBot[2].style.backgroundColor = "transparent";
+    } else if (set.botOption == "gunting") {
+        collectionBot[0].style.backgroundColor = "transparent";
+        collectionBot[1].style.backgroundColor = "red";
+        collectionBot[2].style.backgroundColor = "transparent";
+    }else{
+        collectionBot[0].style.backgroundColor = "transparent";
+        collectionBot[1].style.backgroundColor = "transparent";
+        collectionBot[2].style.backgroundColor = "red";
+    }
     set.name();
     set.startBattle();
     console.log(set.matchResult());
+    if (set.matchResult() !== null) {
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].removeAttribute("onClick");
+        }
+    }
+    let collectionResult = document.getElementsByClassName("result");
+    if (set.startBattle(this.winner) == "player") {
+        collectionResult[0].style.visibility = "hidden";
+        collectionResult[1].style.visibility = "visible";
+        collectionResult[2].style.visibility = "hidden";
+        collectionResult[3].style.visibility = "hidden";
+    } else if (set.startBattle(this.winner) == "binarBot") {
+        collectionResult[0].style.visibility = "hidden";
+        collectionResult[1].style.visibility = "hidden";
+        collectionResult[2].style.visibility = "visible";
+        collectionResult[3].style.visibility = "hidden";
+    } else if(set.startBattle(this) == "SERI") {
+        collectionResult[0].style.visibility = "hidden";
+        collectionResult[1].style.visibility = "hidden";
+        collectionResult[2].style.visibility = "hidden";
+        collectionResult[3].style.visibility = "visible";
+    }
+    return 0;
 }
+
+function resetOption() {
+    const attBatu = document.createAttribute("onclick");
+    const attGunting = document.createAttribute("onclick");
+    const attKertas = document.createAttribute("onclick");
+    attBatu.value = "{ pickOptionPlayer('batu'); }";
+    attGunting.value = "{ pickOptionPlayer('gunting'); }";
+    attKertas.value = "{ pickOptionPlayer('kertas'); }";
+    document.getElementsByClassName("player")[0].setAttributeNode(attBatu);
+    document.getElementsByClassName("player")[1].setAttributeNode(attGunting);
+    document.getElementsByClassName("player")[2].setAttributeNode(attKertas);
+  
+    const collection = document.getElementsByClassName("player");
+    collection[0].style.backgroundColor = "transparent";
+    collection[1].style.backgroundColor = "transparent";
+    collection[2].style.backgroundColor = "transparent";
+  
+    const collection1 = document.getElementsByClassName("bot");
+    collection1[0].style.backgroundColor = "transparent";
+    collection1[1].style.backgroundColor = "transparent";
+    collection1[2].style.backgroundColor = "transparent";
+  }
